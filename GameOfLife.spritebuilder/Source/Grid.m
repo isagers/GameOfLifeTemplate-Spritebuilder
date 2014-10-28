c67d0b51c9a7ccb4881846391a6cca7cb1bb7551
isagers <isabel_gomez7@yahoo.com>
2014-10-28 13:13:31 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -124,6 +124,9 @@ static const int GRID_COLUMNS = 10;
             else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4){
                 currentCreature.isAlive = FALSE;
             }
+            else{
+                currentCreature.isAlive = TRUE;
+            }
         }
     }
 }
