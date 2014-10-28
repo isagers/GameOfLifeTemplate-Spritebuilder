79e47dd7fc34a5234ff0458a7db53951fd9a3206
isagers <isabel_gomez7@yahoo.com>
2014-10-28 13:12:44 +0800

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -117,13 +117,12 @@ static const int GRID_COLUMNS = 10;
 -(void)updateCreatures{
     for(int i = 0; i < _gridArray.count; i++){
         for(int j = 0; j < _gridArray.count; j++){
-            NSLog(_gridArray[i][j]);
             Creature *currentCreature = _gridArray[i][j];
             if(currentCreature.livingNeighbors == 3){
-                currentCreature.isAlive = YES;
+                currentCreature.isAlive = TRUE;
             }
             else if(currentCreature.livingNeighbors <= 1 || currentCreature.livingNeighbors >= 4){
-                currentCreature.isAlive = NO;
+                currentCreature.isAlive = FALSE;
             }
         }
     }
